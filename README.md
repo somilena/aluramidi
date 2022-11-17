220b8b99d2a79696e838c808bba9ef1221922191
somilena <milenliveira@gmail.com>
2022-11-17 07:41:04 -0300

    aluramidi

---
+++ README.md
@@ -0,0 +1,3 @@
+Curso da Alura: JavaScript para Web: Crie páginas dinâmicas.
+
+Fiz somente a parte do JavaScript nesse projeto e curso da Alura.
---
+++ main.js
@@ -0,0 +1,13 @@
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play()
+}
+
+
+
+document.querySelector('.tecla_pom');
+
+document.querySelector('.tecla_pom');
