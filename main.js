926bf29ae1a0730d40d3b6ef1de51a1a4a33206d
somilena <milenliveira@gmail.com>
2022-11-17 11:36:06 -0300

    aluramidi

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play()
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+// enquanto
+while (contador < listaDeTeclas.lenght) {
+
+    listaDeTeclas[contador].onclick = function() {
+        tocaSom('#som_tecla_pom');
+    };
+
+    contador = contador + 1;
+
+    console.log(contador);
+
 }
-
-
-
-document.querySelector('.tecla_pom');
-
-document.querySelector('.tecla_pom');
