a58546f1625b8eb0aa92f3944671f41c5f00005a
somilena <milenliveira@gmail.com>
2022-11-20 11:14:01 -0300

    aluramidi

--- main.js
+++ main.js
@@ -5,20 +5,17 @@ function tocaSom(idElementoAudio) {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 // enquanto
-for (let contador= 0; contador < listaDeTeclas.lenght; contador++) {
+for (let contador= 0; contador < listaDeTeclas.length; contador++) {
     
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
 
     // template string
-    const idAudio = `#som_ ${instrumento}`;
+    const idAudio = `#som_${instrumento}`;
 
-    //console.log(idAudio);
-
+    
     tecla.onclick = function() {
         tocaSom(idAudio);
     };
-
-    //console.log(contador);
 
 }
