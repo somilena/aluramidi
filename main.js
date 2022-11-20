49adf3e8306862f1abda931e379ed9f05f5bc5b4
somilena <milenliveira@gmail.com>
2022-11-20 20:23:52 -0300

    aluramidi

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+            elemento.play();
+    }
+    else {
+        alert('Elemento não encontrado ou seletor inválido!');
+        //console.log('Elemento não encontrado ou seletor inválido!')
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-// enquanto
+// para
 for (let contador= 0; contador < listaDeTeclas.length; contador++) {
     
     const tecla = listaDeTeclas[contador];
@@ -16,6 +24,17 @@ for (let contador= 0; contador < listaDeTeclas.length; contador++) {
     
     tecla.onclick = function() {
         tocaSom(idAudio);
-    };
+    }
+
+    tecla.onkeydown = function(evento) {
+
+       if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
 
 }
